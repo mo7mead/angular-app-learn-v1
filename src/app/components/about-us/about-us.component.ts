87757677d0a26b19ab6ac3e4782a9f606656d89f
mo7mead <mo7mead.wael.dev@gmail.com>
2025-01-06 13:19:22 +0200

    updates

--- src/app/components/about-us/about-us.component.ts
+++ src/app/components/about-us/about-us.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-about-us',
-  imports: [],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.css'
 })
