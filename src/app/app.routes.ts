f3695c6b10996117ecd0d2bf3af5f0edc8ae40bf
mo7mead <mo7mead.wael.dev@gmail.com>
2025-01-07 13:21:36 +0200

    updates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,22 @@ import { HomeComponent } from './components/home/home.component';
 import { ProductsComponent } from './components/products/products.component';
 import { AboutUSComponent } from './components/about-us/about-us.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { VisionComponent } from './components/vision/vision.component';
+import { ValuesComponent } from './components/values/values.component';
 
 export const routes: Routes = [
-
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
     { path: 'products', component: ProductsComponent },
-    { path: 'about', component: AboutUSComponent },
+    {
+        path: 'about', component: AboutUSComponent, children: [
+            { path: '', redirectTo: '/about/vision', pathMatch: 'full' },
+            { path: 'vision', component: VisionComponent },
+            { path: 'values', component: ValuesComponent },
+
+        ],
+
+    },
     { path: '**', component: NotFoundComponent }
 
 ];
